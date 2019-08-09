d5d50f9e4efa3c0348f4cdb60f8b831a5d301996
fusionpit <fusionpit@gmail.com>
2019-08-09 11:47:02 -0600

    add rfc quests

--- QuestsNeverMarkedComplete.lua
+++ QuestsNeverMarkedComplete.lua
@@ -6,6 +6,8 @@ local questsNeverMarkedComplete = {
     40, -- A Fishy Peril
     360, -- Return to the Magistrate
     359, -- Forsaken Duties
+    5729, -- Hidden Enemies
+    5730, -- Hidden Enemies
 }
 
 local questIdMap = {}
